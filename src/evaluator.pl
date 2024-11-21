7ccec9016eaae7454d8ba6d2bda63797e0f523e0
Jean Johnson <jajoh151@asu.edu>
2024-11-21 13:46:17 -0700

    Evaluator code after testing

--- src/evaluator.pl
+++ src/evaluator.pl
@@ -1,6 +1,5 @@
 :- table expr_eval/4.
-
-% EVALUATOR 
+% RUN TIME ENVIRONMENT
 
 % Environment look up -> lookup(Env,Id,Value).
 lookup([[Id,Value]|_],Id,Value).
@@ -15,35 +14,18 @@ update([[X,V]|T],Id,Value,[[X,V]|R]):-
 update([[Id,_]|T],Id,Value,[[Id,Value]|R])
 	:- update(T,1,-1,R).
 
-
 % evaluate program
-program_eval(program(Block),X,Y,Z):-
-    block_eval(Block,[],X,Y,Z).
-
-% evaluate block
-block_eval(block(DeclarationBlock,CommandBlock),Env,X,Y,Z):-
-    declaration_eval(DeclarationBlock,Env,NewEnv1),
-    update(NewEnv1,'x',X,NewEnv2),update(NewEnv2,'y',Y,NewEnv3),
-    command_eval(CommandBlock,NewEnv3,NewEnv4),
-    lookup(NewEnv4,'z',Z).
-
-% evaluate declaration
-declaration_eval(id(X),Env,NewEnv):-update(Env,X,0,NewEnv).
-declaration_eval(X=Y,Env,NewEnv):-update(Env,X,Y,NewEnv).
-declaration_eval(declare(X,Y),Env,NewEnv2):-
-    declaration_eval(X,Env,NewEnv1),
-    declaration_eval(Y,NewEnv1,NewEnv2).
-
+program_eval(program(X),NewEnv):-command_eval(X,[],NewEnv).
 
 % evaluate commands
-command_eval(command(Command1,Command2),Env,NewEnv2):-
+command_eval((Command1,Command2),Env,NewEnv2):-
     command_eval(Command1, Env, NewEnv1),
     command_eval(Command2, NewEnv1, NewEnv2).
 command_eval(X=Expr,Env,NewEnv2):-
     expr_eval(Env,Expr,Value,NewEnv1),
     update(NewEnv1,X,Value,NewEnv2).
 
-% evaluate conditional commands
+% Condition statement
 command_eval(if(condition(BoolExpr),then(Command1),else(_)), Env, NewEnv2):-
     boolean_expr_eval(BoolExpr,Env,NewEnv1,true),
     command_eval(Command1,NewEnv1,NewEnv2).
@@ -62,29 +44,128 @@ command_eval(LoopStatement, Env, NewEnv1):-
     boolean_expr_eval(BoolExpr,Env,NewEnv1,false).
 
 % do while loop
-command_eval(LoopStatement, Env, NewEnv3):-
-    LoopStatement=do_while(body(Command),condition(BoolExpr)),
-    command_eval(Command,NewEnv1,NewEnv2),
+command_eval(do_while(body(Command),condition(BoolExpr)), Env, NewEnv2):-
+    boolean_expr_eval(BoolExpr,Env,NewEnv1,false),
+    command_eval(Command,NewEnv1,NewEnv2).
+command_eval(do_while(body(Command),condition(BoolExpr)), Env, NewEnv3):-
     boolean_expr_eval(BoolExpr,Env,NewEnv1,true),
-    command_eval(LoopStatement,NewEnv2,NewEnv3).
-command_eval(LoopStatement, Env, NewEnv1):-
-    LoopStatement=do_while(body(Command),condition(BoolExpr)),
     command_eval(Command,NewEnv1,NewEnv2),
+    command_eval(do_while_started(body(Command),condition(BoolExpr)),NewEnv2,NewEnv3).
+command_eval(do_while_started(body(Command),condition(BoolExpr)), Env, NewEnv3):-
+    boolean_expr_eval(BoolExpr,Env,NewEnv1,true),
+    command_eval(Command,NewEnv1,NewEnv2),
+    command_eval(do_while_started(body(Command),condition(BoolExpr)),NewEnv2,NewEnv3).
+command_eval(do_while_started(body(_Command),condition(BoolExpr)), Env, NewEnv1):-
+    boolean_expr_eval(BoolExpr,Env,NewEnv1,false).
+
+
+% for loop
+command_eval(LoopStatement, Env, NewEnv5):-
+    LoopStatement=for(start(Command1),condition(BoolExpr),update(Command2),body(Command3)),
+    command_eval(Command1,Env,NewEnv1),
+    boolean_expr_eval(BoolExpr,NewEnv1,NewEnv2,true),
+    command_eval(Command3,NewEnv2,NewEnv3),
+    command_eval(Command2,NewEnv3,NewEnv4),
+    command_eval(for(started,condition(BoolExpr),update(Command2),body(Command3)), NewEnv4, NewEnv5).
+
+command_eval(LoopStatement, Env, NewEnv2):-
+    LoopStatement=for(start(Command1),condition(BoolExpr),update(_),body(_)),
+    command_eval(Command1,Env,NewEnv1),
     boolean_expr_eval(BoolExpr,NewEnv1,NewEnv2,false).
 
+command_eval(LoopStatement, Env, NewEnv4):-
+    LoopStatement=for(started,condition(BoolExpr),update(Command1),body(Command2)),
+    boolean_expr_eval(BoolExpr,Env,NewEnv1,true),
+    command_eval(Command2,NewEnv1,NewEnv2),
+    command_eval(Command1,NewEnv2,NewEnv3),
+    command_eval(LoopStatement, NewEnv3, NewEnv4).
+
+command_eval(LoopStatement, Env, NewEnv1):-
+    LoopStatement=for(started,condition(BoolExpr),update(_),body(_)),
+    boolean_expr_eval(BoolExpr,Env,NewEnv1,false).
+
+% print statement
+command_eval(print_var(X),Env,Env):-
+    lookup(Env,X,Value), write(Value).
+command_eval(print_str(X),_,_):-
+    write(X).
+
 % evaluate boolean expression
-boolean_expr_eval(Expr1=Expr2,Env,NewEnv2,true):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1=Value2.
-boolean_expr_eval(Expr1=Expr2,Env,NewEnv2,false):-
+boolean_expr_eval(Expr1==Expr2,Env,NewEnv2,true):-
+    expr_eval(Env,Expr1,Value,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value,NewEnv2).
+
+boolean_expr_eval(Expr1==Expr2,Env,NewEnv2,false):-
     expr_eval(Env,Expr1,Value1,NewEnv1),
     expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
     Value1\=Value2.
+
+boolean_expr_eval(<=(Expr1,Expr2),Env,NewEnv2,true):-
+    expr_eval(Env,Expr1,Value1,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
+    Value1=<Value2.
+boolean_expr_eval(<=(Expr1,Expr2),Env,NewEnv2,false):-
+    expr_eval(Env,Expr1,Value1,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
+    Value1>Value2.
+
+boolean_expr_eval(Expr1<Expr2,Env,NewEnv2,true):-
+    expr_eval(Env,Expr1,Value1,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
+    Value1<Value2.
+boolean_expr_eval(Expr1<Expr2,Env,NewEnv2,false):-
+    expr_eval(Env,Expr1,Value1,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
+    Value1>=Value2.
+
+boolean_expr_eval(Expr1>Expr2,Env,NewEnv2,true):-
+    expr_eval(Env,Expr1,Value1,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
+    Value1>Value2.
+boolean_expr_eval(Expr1>Expr2,Env,NewEnv2,false):-
+    expr_eval(Env,Expr1,Value1,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
+    Value1=<Value2.
+
+boolean_expr_eval(Expr1>=Expr2,Env,NewEnv2,true):-
+    expr_eval(Env,Expr1,Value1,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
+    Value1>=Value2.
+boolean_expr_eval(Expr1>=Expr2,Env,NewEnv2,false):-
+    expr_eval(Env,Expr1,Value1,NewEnv1),
+    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
+    Value1<Value2.
+
 boolean_expr_eval(not(Expr),Env,NewEnv,false):-
     boolean_expr_eval(Expr, Env, NewEnv,true).
 boolean_expr_eval(not(Expr),Env,NewEnv,true):-
     boolean_expr_eval(Expr, Env, NewEnv,false).
+
+boolean_expr_eval(and(Expr1,Expr2),Env,NewEnv2,true):-
+    boolean_expr_eval(Expr1, Env, NewEnv1,true),
+    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, true).
+boolean_expr_eval(and(Expr1,Expr2),Env,NewEnv2,false):-
+    boolean_expr_eval(Expr1, Env, NewEnv1,true),
+    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, false).
+boolean_expr_eval(and(Expr1,Expr2),Env,NewEnv2,false):-
+    boolean_expr_eval(Expr1, Env, NewEnv1,false),
+    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, true).
+boolean_expr_eval(and(Expr1,Expr2),Env,NewEnv2,false):-
+    boolean_expr_eval(Expr1, Env, NewEnv1,false),
+    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, false).
+
+boolean_expr_eval(or(Expr1,Expr2),Env,NewEnv2,true):-
+    boolean_expr_eval(Expr1, Env, NewEnv1,true),
+    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, true).
+boolean_expr_eval(or(Expr1,Expr2),Env,NewEnv2,true):-
+    boolean_expr_eval(Expr1, Env, NewEnv1,true),
+    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, false).
+boolean_expr_eval(or(Expr1,Expr2),Env,NewEnv2,true):-
+    boolean_expr_eval(Expr1, Env, NewEnv1,false),
+    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, true).
+boolean_expr_eval(or(Expr1,Expr2),Env,NewEnv2,false):-
+    boolean_expr_eval(Expr1, Env, NewEnv1,false),
+    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, false).
 
 % evaluate expression
 expr_eval(Env,Val,Value,Env):-
@@ -107,8 +188,7 @@ expr_eval(Env,X=Expr,Value,NewEnv2):-
     expr_eval(Env,Expr,Value,NewEnv1),
     update(NewEnv1,X,Value,NewEnv2).
 
-
 % evaluate values
 val_eval(_,Id,Id):-number(Id).
 val_eval(Env,Id,Value):- 
-    atom(Id), member(Id,[x,y,z,u,v]), lookup(Env,Id,Value).
+    atom(Id), lookup(Env,Id,Value).
