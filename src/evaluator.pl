299d747cd6b377349006c84226c48763d92c56bd
Jean Johnson <jajoh151@asu.edu>
2024-11-21 13:44:40 -0700

    Evaluator minor modification

--- src/evaluator.pl
+++ src/evaluator.pl
@@ -1,6 +1,6 @@
 :- table expr_eval/4.
 
-% RUN TIME ENVIRONMENT
+% EVALUATOR 
 
 % Environment look up -> lookup(Env,Id,Value).
 lookup([[Id,Value]|_],Id,Value).
@@ -15,7 +15,8 @@ update([[X,V]|T],Id,Value,[[X,V]|R]):-
 update([[Id,_]|T],Id,Value,[[Id,Value]|R])
 	:- update(T,1,-1,R).
 
- % evaluate program
+
+% evaluate program
 program_eval(program(Block),X,Y,Z):-
     block_eval(Block,[],X,Y,Z).
 
@@ -72,81 +73,18 @@ command_eval(LoopStatement, Env, NewEnv1):-
     boolean_expr_eval(BoolExpr,NewEnv1,NewEnv2,false).
 
 % evaluate boolean expression
-boolean_expr_eval(Expr1==Expr2,Env,NewEnv2,true):-
+boolean_expr_eval(Expr1=Expr2,Env,NewEnv2,true):-
     expr_eval(Env,Expr1,Value1,NewEnv1),
     expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
     Value1=Value2.
-boolean_expr_eval(Expr1==Expr2,Env,NewEnv2,false):-
+boolean_expr_eval(Expr1=Expr2,Env,NewEnv2,false):-
     expr_eval(Env,Expr1,Value1,NewEnv1),
     expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
     Value1\=Value2.
-
-boolean_expr_eval(<=(Expr1,Expr2),Env,NewEnv2,true):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1=<Value2.
-boolean_expr_eval(<=(Expr1,Expr2),Env,NewEnv2,false):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1>Value2.
-
-boolean_expr_eval(Expr1<Expr2,Env,NewEnv2,true):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1<Value2.
-boolean_expr_eval(Expr1<Expr2,Env,NewEnv2,false):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1>=Value2.
-
-boolean_expr_eval(Expr1>Expr2,Env,NewEnv2,true):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1>Value2.
-boolean_expr_eval(Expr1>Expr2,Env,NewEnv2,false):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1=<Value2.
-
-boolean_expr_eval(Expr1>=Expr2,Env,NewEnv2,true):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1>=Value2.
-boolean_expr_eval(Expr1>=Expr2,Env,NewEnv2,false):-
-    expr_eval(Env,Expr1,Value1,NewEnv1),
-    expr_eval(NewEnv1,Expr2,Value2,NewEnv2),
-    Value1<Value2.
-
 boolean_expr_eval(not(Expr),Env,NewEnv,false):-
     boolean_expr_eval(Expr, Env, NewEnv,true).
 boolean_expr_eval(not(Expr),Env,NewEnv,true):-
     boolean_expr_eval(Expr, Env, NewEnv,false).
-
-boolean_expr_eval(and(Expr1,Expr2),Env,NewEnv2,true):-
-    boolean_expr_eval(Expr1, Env, NewEnv1,true),
-    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, true).
-boolean_expr_eval(and(Expr1,Expr2),Env,NewEnv2,false):-
-    boolean_expr_eval(Expr1, Env, NewEnv1,true),
-    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, false).
-boolean_expr_eval(and(Expr1,Expr2),Env,NewEnv2,false):-
-    boolean_expr_eval(Expr1, Env, NewEnv1,false),
-    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, true).
-boolean_expr_eval(and(Expr1,Expr2),Env,NewEnv2,false):-
-    boolean_expr_eval(Expr1, Env, NewEnv1,false),
-    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, false).
-
-boolean_expr_eval(or(Expr1,Expr2),Env,NewEnv2,true):-
-    boolean_expr_eval(Expr1, Env, NewEnv1,true),
-    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, true).
-boolean_expr_eval(or(Expr1,Expr2),Env,NewEnv2,true):-
-    boolean_expr_eval(Expr1, Env, NewEnv1,true),
-    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, false).
-boolean_expr_eval(or(Expr1,Expr2),Env,NewEnv2,true):-
-    boolean_expr_eval(Expr1, Env, NewEnv1,false),
-    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, true).
-boolean_expr_eval(or(Expr1,Expr2),Env,NewEnv2,false):-
-    boolean_expr_eval(Expr1, Env, NewEnv1,false),
-    boolean_expr_eval(Expr2, NewEnv1, NewEnv2, false).
 
 % evaluate expression
 expr_eval(Env,Val,Value,Env):-
@@ -169,7 +107,8 @@ expr_eval(Env,X=Expr,Value,NewEnv2):-
     expr_eval(Env,Expr,Value,NewEnv1),
     update(NewEnv1,X,Value,NewEnv2).
 
+
 % evaluate values
 val_eval(_,Id,Id):-number(Id).
 val_eval(Env,Id,Value):- 
-    atom(Id), lookup(Env,Id,Value).
+    atom(Id), member(Id,[x,y,z,u,v]), lookup(Env,Id,Value).
